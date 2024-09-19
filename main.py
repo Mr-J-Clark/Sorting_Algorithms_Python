d9257bdda352e4c6790c92418c5f55a0237345e6
Mr-J-Clark <jclark@bisphuket.ac.th>
2024-09-19 11:30:53 +0700

    updated

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 print ("sorting algorithms.... selection and bubble!")
 print ("here we go")
-unsorted = [23, 87, 56, 12, 78, 45, 90, 34, 67, 89, 41, 72, 54, 15, 63, 98, 22, 11, 77, 36]
 sorted = []
 print (f"unsorted list: {unsorted}")
 
-while len(unsorted) > 0:
-    lowest = unsorted[0]
-    for value in unsorted:
-        if value < lowest:
-            lowest = value
-    sorted.append(lowest)
-    unsorted.remove(lowest)
+# selection sort into new list
+# while len(unsorted) > 0:
+#     lowest = unsorted[0]
+#     for value in unsorted:
+#         if value < lowest:
+#             lowest = value
+#     sorted.append(lowest)unsorted = [23, 87, 56, 12, 78, 45, 90, 34, 67, 89, 41, 72, 54, 15, 63, 98, 22, 11, 77, 36]
+#     unsorted.remove(lowest)
+# print (sorted)
 
-print (sorted)
+#bubble sort.
 
+for j in range (len(unsorted)): #looping multiple passes
+    for i in range (len(unsorted)-1): # each pass
+        if unsorted[i] > unsorted [i+1]: # if the current item is > the next
+            temp = unsorted[i] #make a place-holder for the current item so we don't "lose it"
+            unsorted[i] = unsorted [i+1] # the current value is replaced with the next
+            unsorted [i+1] = temp # the next is overwritten with the temp (that we didn't lose)
+print (unsorted)
 
 
 
