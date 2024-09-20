09f0dec739e898189e8ebb712e9f0fc2ca9ecfd8
Mr-J-Clark <jclark@bisphuket.ac.th>
2024-09-20 08:42:47 +0700

    updated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 print ("sorting algorithms.... selection and bubble!")
 print ("here we go")
+unsorted =  [23, 87, 56, 12, 78, 45, 90, 34, 67, 89, 41, 72, 54, 15, 63, 98, 22, 11, 77, 36]
+
 sorted = []
 print (f"unsorted list: {unsorted}")
 
@@ -14,15 +16,50 @@ print (f"unsorted list: {unsorted}")
 # print (sorted)
 
 #bubble sort.
-
+count = 0
 for j in range (len(unsorted)): #looping multiple passes
+    count += 1
+
     for i in range (len(unsorted)-1): # each pass
         if unsorted[i] > unsorted [i+1]: # if the current item is > the next
             temp = unsorted[i] #make a place-holder for the current item so we don't "lose it"
             unsorted[i] = unsorted [i+1] # the current value is replaced with the next
             unsorted [i+1] = temp # the next is overwritten with the temp (that we didn't lose)
+            count += 1
+        count += 1
+
+
+
+print (unsorted)
+print (f"normal bubble sort = {count}")
+
+#bubble sort MORE EFFICIENT.
+
+unsorted =  [23, 87, 56, 12, 78, 45, 90, 34, 67, 89, 41, 72, 54, 15, 63, 98, 22, 11, 77, 36]
+
+print ("bubble sort MORE EFFICIENT.")
+count = 0
+
+for j in range (len(unsorted)): #looping multiple passe
+    sorted = True
+    count += 1
+    for i in range (len(unsorted)-1): # each pass
+
+        if unsorted[i] > unsorted [i+1]: # if the current item is > the next
+            temp = unsorted[i] #make a place-holder for the current item so we don't "lose it"
+            unsorted[i] = unsorted [i+1] # the current value is replaced with the next
+            unsorted [i+1] = temp # the next is overwritten with the temp (that we didn't lose)
+            sorted = False
+            count += 1
+
+    if sorted == True:
+        break
+
+print (f"efficient = {count}")
 print (unsorted)
 
+print (f"efficient = {count}")
+print (unsorted)
 
 
 
